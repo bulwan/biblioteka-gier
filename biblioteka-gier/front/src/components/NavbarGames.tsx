47403a7aa2b1a32b32543490586d3a452e4523b3
bulwan <joanna_bulwan@o2.pl>
2023-08-08 09:38:49 +0200

    new gitignore

--- biblioteka-gier/front/src/components/NavbarGames.tsx
+++ biblioteka-gier/front/src/components/NavbarGames.tsx
@@ -1,14 +1,17 @@
-
-import "./Navbar.scss"
+import "./Navbar.css";
 const NavbarGames = (props: any) => {
-  return (
-    <div className="gameList__container">
-      <div className="gameList__gameCard" id={props.key}>
-        <div className="gameCard__image"><img src={props.image}/></div>
-        <div className="gameCard__title">{props.title}</div>
-        <div className="gameCard__platforms"><img src={props.platforms}/></div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="gameList__container">
+			<div className="gameList__gameCard" id={props.key}>
+				<div className="gameCard__image">
+					<img src={props.image} />
+				</div>
+				<div className="gameCard__title">{props.title}</div>
+				<div className="gameCard__platforms">
+					<img src={props.platforms} />
+				</div>
+			</div>
+		</div>
+	);
 };
 export default NavbarGames;
