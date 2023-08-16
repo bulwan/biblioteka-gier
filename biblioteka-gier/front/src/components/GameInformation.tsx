d1bd74ac4814f0940a989c09f8331dfb9c724442
bulwan <joanna_bulwan@o2.pl>
2023-08-16 13:29:27 +0200

    added basic memorization

--- biblioteka-gier/front/src/components/GameInformation.tsx
+++ biblioteka-gier/front/src/components/GameInformation.tsx
@@ -5,118 +5,132 @@ import Sidebar from "./Sidebar";
 import "../pages/gameDetails/gameDetails.css";
 
 type gameInformationProps = {
-  id: any;
-  image: string;
-  rating: string;
-  title: string;
-  platforms: string;
-  description: string; 
+    id: any;
+    image: string;
+    rating: string;
+    title: string;
+    platforms: string;
+    description: string;
 };
 
 const GameInformation: React.FC<gameInformationProps> = () => {
-  const location = useLocation();
-  const game = location.state?.game;
-  const { id: gameId } = useParams();
-  const [gameInfo, setGameInfo] = useState<any>(null);
+    const location = useLocation();
+    const game = location.state?.game;
+    const { id: gameId } = useParams();
+    const [gameInfo, setGameInfo] = useState<any>(null);
+    //const [images, setImages] = useState({});
 
-  useEffect(() => {
-    axios
-      .get(`https://api.rawg.io/api/games/${gameId}?key=2b4ab8ba5c2342b180e48f08be47c96d`)
-      .then((response) => {
-        setGameInfo(response.data);
-      })
-      .catch((error) => {
-        console.error("Błąd pobierania informacji o grze:", error);
-      });
-  }, [gameId]);
+    useEffect(() => {
+        if (localStorage.getItem(gameId)) {
+            let newGame = localStorage.getItem(gameId);
+            setGameInfo(JSON.parse(newGame));
+            console.log("NIE MA ZAPYTANIA");
+        } else {
+            console.log("JEST ZAPYTANIE");
+            axios
+                .get(`https://api.rawg.io/api/games/${gameId}?key=63a39ad053c946faa4c38e1b26c581e4`)
+                .then((response) => {
+                    setGameInfo(response.data);
+                    localStorage.setItem(gameId, JSON.stringify(response.data));
+                    axios
+                        .get(
+                            `https://api.rawg.io/api/games/${gameId}/screenshots?key=63a39ad053c946faa4c38e1b26c581e4`
+                        )
+                        .then((response) => {
+                            const cacheGame = JSON.parse(localStorage.getItem(gameId));
+                            const newData = {
+                                ...cacheGame,
+                                screenshots: { ...response.data.results },
+                            };
+                            localStorage.setItem(gameId, JSON.stringify(newData));
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        });
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    }, [gameId]);
+    //console.log(gameInfo.screenshots);
+    return (
+        <div className="gameDetails__fullPage">
+            <Sidebar />
+            <div className="gameDetails">
+                {gameInfo && (
+                    <div key={gameInfo.id} className="gameDetails__container">
+                        <div className="gameDetails__image">
+                            <img src={game.background_image} alt={game.name} />
+                        </div>
+                        <div className="gameDetails__rating">{game.metacritic}</div>
+                        <div className="gameDetails__title">{game.name}</div>
+                        <div className="gameDetails__infoContainer">
+                            <div className="infoContainer__releaseDate">
+                                <h1>Release date</h1>
+                                <p>{gameInfo.released}</p>
+                            </div>
+                            <div className="infoContainer__developer">
+                                <h1>Developers</h1>
+                                <p>
+                                    {gameInfo.developers
+                                        ? gameInfo.developers.map((developer: { name: string }) => (
+                                              <p>{developer.name}</p>
+                                          ))
+                                        : ""}
+                                </p>
+                            </div>
+                            <div className="infoContainer__platforms">
+                                <h1></h1>
+                                <div className="platforms__images"></div>
+                            </div>
+                            <div className="infoContainer__genre">
+                                <h1>Genre</h1>
+                                <div className="genre__names">
+                                    {gameInfo.genres
+                                        ? gameInfo.genres.map((genres: { name: string }) => (
+                                              <p>{genres.name}</p>
+                                          ))
+                                        : ""}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="gameCard__addToCollections">
+                            <div className="gameCard__addToCollections-dropdown">
+                                <div className="gameCard__addToCollections-button">
+                                    Change a status <span>+</span>
+                                </div>
+                                <div className="gameCard__addToCollections-content">
+                                    <div>Completed</div>
+                                    <div>In plans</div>
+                                    <div>Playing</div>
+                                    <div>Abandoned</div>
+                                </div>
+                            </div>
+                            <div className="gameCard__addToCollections-dropdown">
+                                <div className="gameCard__addToCollections-button">
+                                    Add to collections <span>+</span>
+                                </div>
+                                <div className="gameCard__addToCollections-content">
+                                    <div className="dropdown-custom">Create collection</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="gameDetails__screenshotsTitle">
+                            <h1>Screenshots</h1>
+                        </div>
 
-  return (
-    <div className="gameDetails__fullPage">
-      <Sidebar />
-      <div className="gameDetails">
-        {gameInfo && (
-          <div key={gameInfo.id} className="gameDetails__container">
-            <div className="gameDetails__image">
-              <img src={game.background_image} alt={game.name} />
+                        <div className="gameDetails__screenshotsContainer"></div>
+                        <div className="gameDetails_descriptionTitle">
+                            <h1>About</h1>
+                        </div>
+                        <div className="gameDetails_description">
+                            <h1>{gameInfo.description_raw}</h1>
+                        </div>
+                    </div>
+                )}
             </div>
-            <div className="gameDetails__rating">{game.metacritic}</div>
-            <div className="gameDetails__title">{game.name}</div>
-            <div className="gameDetails__infoContainer">
-              <div className="infoContainer__releaseDate">
-                <h1>Release date</h1>
-                <p>{gameInfo.released}</p>
-              </div>
-              <div className="infoContainer__developer">
-                <h1>Developers</h1>
-                <p>
-                  {gameInfo.developers
-                    ? gameInfo.developers.map(
-                        (developer: { name: string }) => developer.name
-                      )
-                    : ""}
-                </p>
-              </div>
-              <div className="infoContainer__platforms">
-              <h1></h1>
-              <div className="platforms__images"></div>
-            </div>
-            <div className="infoContainer__genre">
-              <h1>Genre</h1>
-              <div className="genre__names">
-                <p>FPS</p>
-                <p>RPG</p>
-                <p>Adenture</p>
-              </div>
-            </div>
-          </div>
-          <div className="gameDetails__buttonContainer">
-            <div className="buttonContainer__completed">
-              <button>
-                <img src="src\images\completed-icon.png" alt="completed-icon" />
-                Completed
-              </button>
-            </div>
-            <div className="buttonContainer__playing">
-              <button>
-                <img src="src\images\playing-icon.png" alt="playing-icon" />
-                Playing
-              </button>
-            </div>
-            <div className="buttonContainer__inPlans">
-              <button>
-                <img src="src\images\inPlans-icon.png" alt="inPlans-icon" />
-                In plans
-              </button>
-            </div>
-            <div className="buttonContainer__abandoned">
-              <button>
-                <img src="src\images\abandoned-icon.png" alt="abandoned-icon" />
-                Abandoned
-              </button>
-            </div>
-          </div>
-          <div className="gameDetails__addToLibrary">
-            <button>
-              Add to library
-              <img
-                src="src\images\addToLibrary-icon.png"
-                alt="abandoned-icon"
-              />
-            </button>
-          </div>
-          <div className="gameDetails__screenshotsTitle">
-            <h1>Screenshots</h1>
-          </div>
-          <div className="gameDetails__screenshotsContainer"></div>
-          <div className="gameDetails_descriptionTitle">
-            <h1>About</h1>
-          </div>
-          <div className="gameDetails_description">
-            <h1>{gameInfo.description}</h1>
-          </div>
         </div>
-        )}
-        </div>
-    </div>
-)};
+    );
+};
 export default GameInformation;
--- biblioteka-gier/front/src/pages/home/Home.tsx
+++ biblioteka-gier/front/src/pages/home/Home.tsx
@@ -7,73 +7,70 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Loading from "../../components/Loading";
 import { Link } from "react-router-dom";
 function Home() {
-  const [gamecard, setGamecard] = useState<any>([]);
-  const [error, setError] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
+    const [gamecard, setGamecard] = useState<any>([]);
+    const [error, setError] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
 
-  const fetchData = () => {
-    axios
-      .get(
-        `https://api.rawg.io/api/games?key=2b4ab8ba5c2342b180e48f08be47c96d&page=${currentPage}&page_size=15&metacritic=70,100`
-      )
-      .then((response) => {
-        setTimeout(() => {
-          setGamecard((prevData: any) => [
-            ...prevData,
-            ...response.data.results,
-          ]);
-          setCurrentPage(currentPage + 1);
-        }, 500);
-      })
-      .catch((error) => {
-        setError(error);
-        console.log(error);
-      });
-    console.log(gamecard);
-  };
+    const fetchData = () => {
+        axios
+            .get(
+                `https://api.rawg.io/api/games?key=63a39ad053c946faa4c38e1b26c581e4&page=${currentPage}&page_size=15&metacritic=70,100`
+            )
+            .then((response) => {
+                const newGames = gamecard;
+                for (const game of response.data.results) {
+                    newGames.push(game);
+                    // localStorage.setItem(game.id, JSON.stringify(game));
+                }
+                setGamecard(newGames);
+                setCurrentPage(() => currentPage + 1);
+            })
+            .catch((error) => {
+                setError(error);
+                console.log(error);
+            });
+    };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  return (
-    <InfiniteScroll
-      dataLength={gamecard.length}
-      next={fetchData}
-      hasMore={true}
-      loader={<Loading />}
-      endMessage={<p>No more data to load.</p>}
-    >
-      <div className="home">
-        <div className="home__mainElements">
-          <Sidebar />
-          <div className="home__mainContainer">
-            <OrderBy />
-            <div className="home__gameContainer">
-              {gamecard.map((elements: any) => (
-                <Link
-				to={`/game/${elements.id}`}
-				state={{ game: elements }}
-				key={elements.id}
-				className="gameContainer__link"
-			  >
-                  <GameCard
-                    key={elements.id}
-                    id={elements.id}
-                    image={elements.background_image}
-                    rating={elements.metacritic}
-                    title={elements.name}
-                    developers={elements.developers}
-                    platforms={elements.platforms}
-                  />
-                </Link>
-              ))}
+    return (
+        <InfiniteScroll
+            dataLength={gamecard.length}
+            next={fetchData}
+            hasMore={true}
+            loader={<Loading />}
+            endMessage={<p>No more data to load.</p>}>
+            <div className="home">
+                <div className="home__mainElements">
+                    <Sidebar />
+                    <div className="home__mainContainer">
+                        <OrderBy />
+                        <div className="home__gameContainer">
+                            {gamecard.map((elements: any) => (
+                                <Link
+                                    to={`/game/${elements.id}`}
+                                    state={{ game: elements }}
+                                    key={elements.id}
+                                    className="gameContainer__link">
+                                    <GameCard
+                                        key={elements.id}
+                                        id={elements.id}
+                                        image={elements.background_image}
+                                        rating={elements.metacritic}
+                                        title={elements.name}
+                                        developers={elements.developers}
+                                        platforms={elements.platforms}
+                                    />
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </InfiniteScroll>
-  );
+        </InfiniteScroll>
+    );
 }
 
 export default Home;
